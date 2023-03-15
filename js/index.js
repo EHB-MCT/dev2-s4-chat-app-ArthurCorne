1323b6c1b84a4f9b5e341470c35c2d7eefd99008
ArthurCorne <56119522+ArthurCorne@users.noreply.github.com>
2023-03-15 14:08:20 +0100

    Chat app fetch van api

--- js/index.js
+++ js/index.js
@@ -5,12 +5,22 @@ const chat = {
     init() {
     },
     sendMessage() {
+        this.fetchMessages();
     },
     fetchMessages() {
+        fetch ('https://dev2chat.onrender.com/messages')
+        .then(function(response){
+    return response.json();
+    })
+    .then(function(messages){
+console.log(messages);
+
+    });
     },
+    
     renderMessage(message) {
     }
 
-}
+};
 
 chat.init();
