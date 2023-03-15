2e6b11c279f8e551347643bce1a685ed7e282a66
ArthurCorne <56119522+ArthurCorne@users.noreply.github.com>
2023-03-15 14:55:16 +0100

    render message

--- js/index.js
+++ js/index.js
@@ -8,17 +8,30 @@ const chat = {
         this.fetchMessages();
     },
     fetchMessages() {
-        fetch ('https://dev2chat.onrender.com/messages')
-        .then(function(response){
-    return response.json();
-    })
-    .then(function(messages){
-console.log(messages);
+        fetch('https://dev2chat.onrender.com/messages')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (messages) {
+                console.log(messages);
+                const message = messages[0];
+                console.log(message);
 
-    });
+            });
     },
-    
+
     renderMessage(message) {
+        let htmlstring = `
+        <div class="messageItem">
+                  <div class="header">
+                      <span class="author">${message.author}</span>
+                      <span class="time">00:00</span>
+                  </div>
+                  <p>
+                  ${message.message}
+                  </p>
+              </div>`;
+              document.querySelector("#messageContainer").insertAdjecentHTML('beforeend',htmlstring);
     }
 
 };
